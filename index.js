caad74ff9d6c8fb4a9251f6b3b073abee583a9e7
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2021-01-27 16:47:19 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(stringsArr, event){
+  let newStrings = []
+
+  for(let i = 0; i < stringsArr.length; i++){
+    newStrings.push(`Thank you, ${stringsArr[i]}, for the wonderful ${event} gift!`)
+  }
+
+  return newStrings
+}
+
+
+function countDown(int){
+  let i = int;
+
+  while (i >= 0){
+    console.log(i--)
+  }
+}
